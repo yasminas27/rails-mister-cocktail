812c05a89d4308b44eb48e3e9fe2dab2fef178be
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-18 15:38:27 +0200

    controller doses updated

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,9 +6,11 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     @dose.save
     if @dose.save
-      redirect_to cocktail_doses_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
